f9ac7f24e4cd5065108ead5e362506e1688507d9
misfitpixel <brandin@misfitpixel.io>
2024-12-10 12:01:55 -0500

    cleanup

---
+++ src/components/navbar.stories.ts
@@ -0,0 +1,13 @@
+import type {Meta, StoryObj} from "@storybook/react";
+import Navbar from "@/components/navbar";
+
+const meta: Meta<typeof Navbar> = {
+  title: "Design System/Navbar",
+  component: Navbar
+}
+
+export default meta;
+
+type Story = StoryObj<typeof Navbar>;
+
+export const Basic: Story = {};
